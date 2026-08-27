744cda3e1c823c83413eee3a454a65f6f0cc86d1
vciacg <vciaranfi@gmail.com>
2026-08-27 21:36:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // connessione instabile, MA senza mai mettere in cache i dati meteo, che
 // devono sempre essere richiesti freschi dalla rete.
 
-const CACHE_NAME = 'calmflight-shell-v6';
+const CACHE_NAME = 'calmflight-shell-v7';
 
 const APP_SHELL = [
   './',
